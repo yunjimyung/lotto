323b9d8a126cec97ecdaccbe0015dfbe0106d7fb
동력차-14\부곡차량 <yunjimyung@gmail.com>
2017-09-01 16:25:21 +0900

    번호선택(당첨번호와 비교, 3이하로 겹치는번호)

--- lotto_prediction.py
+++ lotto_prediction.py
@@ -1,13 +1,44 @@
 import csv
+import random
 
-with open('./lotto_data.csv','r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        lst = []
-        for i in range(49):
-            lst.append(0)
-        for j in row:
-            lst[int(j)-1] = 1
-        with open('./lotto_learning.csv','a') as w:
-            Writer = csv.writer(w)
-            Writer.writerow(lst)
+def make_data():
+    with open('./lotto_data.csv','r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            lst = []
+            for i in range(51):
+                lst.append(0)
+            for j in row:
+                lst[int(j)-1] = 1
+                lst[49] = 1
+            with open('./lotto_learning.csv','a',newline ='') as w:
+                Writer = csv.writer(w)
+                Writer.writerow(lst)
+
+#matke prediction lotto number1
+def make_number1():
+    number_list = []
+    while len(number_list) < 6:
+         random_number = random.randrange(1,46)
+         if random_number in number_list:
+             continue
+         number_list.append(random_number)
+         number_list.sort()
+    result = number_list
+    return result
+
+def make_number2():
+    with open('./lotto_data.csv','r') as f:
+        reader = csv.reader(f)
+        temp1 = make_number1()
+        for row in reader:
+            temp2 = [int(i)for i in row]
+            temp3 = list(set(temp1) - set(temp2))
+            while len(temp3) < 3:
+                temp1 = make_number1()
+                temp3 = list(set(temp1) - set(temp2))
+        return temp1
+
+if __name__ == "__main__":
+    result = make_number2()
+    print(result)
--- matplotlib.py
+++ matplotlib.py
@@ -1,7 +1,6 @@
-import matplotliv.puplot as plt
+import matplotlib.pyplot as plt
 X=[1,2,3,4,5,6,7,8,9,10]
 Y=[1,2,4,4,5,6,7,8,9,10]
-X on the x-axis
-Y on the y-axis
 plt.plot(X,Y)
 plt.show()
+print(X+Y)
